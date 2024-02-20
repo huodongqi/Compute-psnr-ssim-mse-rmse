276486febee5d15280239d44195409ec0500d3fb
HDQ <38202286+huodongqi@users.noreply.github.com>
2024-02-20 11:16:33 +0800

    Add files via upload

--- psnr.py
+++ psnr.py
@@ -1,6 +1,7 @@
 import cv2
 import os
-from skimage.measure import compare_ssim
+#from skimage.measure import compare_ssim
+from skimage.metrics import structural_similarity as compare_ssim
 
 sr_dir = os.listdir('./SR')
 hr_dir = os.listdir('./HR')
@@ -8,6 +9,7 @@ hr_dir = os.listdir('./HR')
 psnr = 0.0
 ssim = 0.0
 n = 0
+
 
 def to_grey(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -20,6 +22,11 @@ for hr_image in hr_dir:
             compute_psnr = cv2.PSNR(cv2.imread('./SR/' + sr_image), cv2.imread('./HR/' + hr_image))
             compute_ssim = compare_ssim(to_grey(cv2.imread('./SR/' + sr_image)),
                                         to_grey(cv2.imread('./HR/' + hr_image)))
+            
+            print(sr_image + ' PSNR = ', compute_psnr)
+            print(sr_image + ' SSIM = ', compute_ssim)
+            print("++++++++++++++++++++++++++++")
+
             psnr += compute_psnr
             ssim += compute_ssim
             n += 1
@@ -30,4 +37,5 @@ psnr = psnr / n
 ssim = ssim / n
 print("average psnr = ", psnr)
 print("average ssim = ", ssim)
+print("count = ", n)
 
